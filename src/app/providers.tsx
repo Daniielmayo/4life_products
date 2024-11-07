af1a3cb5479af3ce0d780dda1e70051d5d0e6fae
daniel mayo <daniielmayo.mayo@gmail.com>
2024-11-07 12:57:19 -0500

    chage imprt file tailwind config

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -3,7 +3,6 @@
 import * as React from "react";
 import { NextUIProvider } from "@nextui-org/system";
 import { useRouter } from "next/navigation";
-import { ThemeProvider as NextThemesProvider } from "next-themes";
 
 export interface ProvidersProps {
   children: React.ReactNode;
