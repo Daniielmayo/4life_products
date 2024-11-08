7742b68405c4c4bd6bbed85a29e01cce8a4d8427
daniel mayo <daniielmayo.mayo@gmail.com>
2024-11-08 10:33:52 -0500

    update dapendencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,16 @@
-import {nextui} from "@nextui-org/react";
+// tailwind.config.js
+const {nextui} = require("@nextui-org/react");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}'
+    // ...
+    // make sure it's pointing to the ROOT node_module
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
   darkMode: "class",
   plugins: [nextui()],
-}
+};
